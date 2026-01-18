0177d3a9f01cf194fb807e0be4ba556106c91813
Jaxxman007 <kingvikkie@gmail.com>
2026-01-18 18:13:41 +0100

    Update Farm.js

--- models/Farm.js
+++ models/Farm.js
@@ -5,8 +5,9 @@ const farmSchema = new mongoose.Schema(
  size_hectares: { type: Number, default: 0 }, 
  primary_crops: [{ type: String }], 
  livestock: [{ type: String }] }, 
- farm_size: [{ type: string}], { timestamps: true }
+ farm_size: [{ type: String}], { timestamps: true }
 );
 
 module.exports = mongoose.model("Farm", farmSchema);
+
 
